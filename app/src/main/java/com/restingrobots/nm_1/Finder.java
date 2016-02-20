f8a5371a7484bcb9da4bbab733e89fc6a159be06
Andriy <thepan0301@gmail.com>
2016-02-20 14:36:04 +0200

    Бавлюсь

--- app/src/main/java/com/restingrobots/nm_1/Finder.java
+++ app/src/main/java/com/restingrobots/nm_1/Finder.java
@@ -17,7 +17,7 @@ public class Finder {
             while(true) {
                 x = (To + From)/2f;
                 if(Math.abs(To - From) < Eps || getY(x) == 0) {
-                    return ("Поділу: " + new BigDecimal(x).setScale(3, RoundingMode.HALF_UP).doubleValue() + "; " + i);
+                    return ("Поділу: х = " + new BigDecimal(x).setScale(3, RoundingMode.HALF_UP).doubleValue() + "; і = " + i);
                 }
                 else {
                     if(getY(From)*getY(x) < 0) {
@@ -41,7 +41,7 @@ public class Finder {
             int i = 0;
             if(Math.abs(To - From) < Eps) {
                 x = (To + From)/2f;
-                return ("Хорд: " + new BigDecimal(x).setScale(3, RoundingMode.HALF_UP).doubleValue() + "; " + i);
+                return ("Хорд: х = " + new BigDecimal(x).setScale(3, RoundingMode.HALF_UP).doubleValue() + "; і = " + i);
             }
             if(!(getYDD(To)*getY(To) > 0)) {
                 double temp = From;
@@ -51,7 +51,7 @@ public class Finder {
             while(true) {
                 x = From - (getY(From)*(To-From)/(getY(To)-getY(From)));
                 if(Math.abs(x - From) < Eps) {
-                    return ("Хорд: " + new BigDecimal(x).setScale(3, RoundingMode.HALF_UP).doubleValue() + "; " + i);
+                    return ("Хорд: х = " + new BigDecimal(x).setScale(3, RoundingMode.HALF_UP).doubleValue() + "; і = " + i);
                 }
                 else {
                     From = x;
@@ -70,7 +70,7 @@ public class Finder {
             int i = 0;
             if(Math.abs(To - From) < Eps) {
                 x = (To + From)/2f;
-                return ("Ньютона: " + new BigDecimal(x).setScale(3, RoundingMode.HALF_UP).doubleValue() + "; " + i);
+                return ("Ньютона: х = " + new BigDecimal(x).setScale(3, RoundingMode.HALF_UP).doubleValue() + "; і = " + i);
             }
             if(!(getYDD(To)*getY(To) > 0)) {
                 double temp = From;
@@ -80,7 +80,7 @@ public class Finder {
             while(true) {
                 x = To - getY(To)/getYD(To);
                 if(Math.abs(To - x) < Eps) {
-                    return ("Ньютона: " + new BigDecimal(x).setScale(3, RoundingMode.HALF_UP).doubleValue() + "; " + i);
+                    return ("Ньютона: х = " + new BigDecimal(x).setScale(3, RoundingMode.HALF_UP).doubleValue() + "; і = " + i);
                 }
                 else {
                     To = x;
@@ -100,13 +100,13 @@ public class Finder {
             int i = 0;
             if(Math.abs(To - From) < Eps) {
                 x = (To + From)/2f;
-                return ("Ітерацій: " + new BigDecimal(x).setScale(3, RoundingMode.HALF_UP).doubleValue() + "; " + i);
+                return ("Ітерацій: х = " + new BigDecimal(x).setScale(3, RoundingMode.HALF_UP).doubleValue() + "; і = " + i);
             }
             x0 = (To + From)/2f;
             while(true) {
                 x = getFi(x0);
                 if(Math.abs(x - x0) < Eps) {
-                    return ("Ітерацій: " + new BigDecimal(x).setScale(3, RoundingMode.HALF_UP).doubleValue() + "; " + i);
+                    return ("Ітерацій: х = " + new BigDecimal(x).setScale(3, RoundingMode.HALF_UP).doubleValue() + "; і = " + i);
                 }
                 else {
                     x0 = x;
